c2bd1260dfb2aff6aff112805e358244caa3a58f
Bass Rhombus <bassrhombus4016@gmail.com>
2025-03-02 02:13:14 -0600

    refactor server routes and add mod manager functionality; implement mod loading and configuration generation

--- server.js
+++ server.js
@@ -3,40 +3,30 @@ const path = require('path');
 const app = express();
 const PORT = 3003;
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from root directory
+app.use(express.static(__dirname));
 
-// Serve the main HTML pages
+// Special route for mod-manager.html that serves the ModINI content
+app.get('/mod-manager.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ModINI/public/index.html'));
+});
+
+// Make ModINI assets available
+app.use('/mod-manager', express.static(path.join(__dirname, 'ModINI/public')));
+
+// Default route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// Catch-all for other HTML pages
+app.get('/:page.html', (req, res) => {
+  // Skip mod-manager.html as it's handled above
+  if (req.params.page !== 'mod-manager') {
+    res.sendFile(path.join(__dirname, req.params.page + '.html'));
+  }
 });
 
-app.get('/mod-manager.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'mod-manager.html'));
-});
-
-app.get('/community.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'community.html'));
-});
-
-app.get('/about.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'about.html'));
-});
-
-app.get('/login.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
-});
-
-app.get('/bots.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'bots.html'));
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`DinoMod Hub server running at http://localhost:${PORT}/`);
-  console.log(`Press Ctrl+C to stop the server`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
