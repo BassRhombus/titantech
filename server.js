88e63f181d86873e2746590bf086ae5d2c67a9e4
Bass Rhombus <bassrhombus4016@gmail.com>
2025-04-14 00:11:37 -0500

    fix: update CALLBACK_URL for production environment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 3003;
 const DISCORD_CLIENT_ID = '1346303613819027496';
 const DISCORD_CLIENT_SECRET = 'ilNi2fsknANLfjLf-bMdW9qoTJEMIvXk';
 const CALLBACK_URL = process.env.NODE_ENV === 'production' 
-  ? `https://${process.env.RENDER_EXTERNAL_HOSTNAME}/auth/discord/callback` 
+  ? 'https://titantech.party/auth/discord/callback' 
   : 'http://localhost:3003/auth/discord/callback';
 
 // List of Discord user IDs with admin privileges
